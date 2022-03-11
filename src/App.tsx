d7d27c3be21d6c4fc3b3cafa2e8e37eca5896ae9
1catchman <msgforcatchman@gmail.com>
2022-03-11 17:18:07 +0600

    update app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { ThemeProvider } from '@mui/material/styles';
 import { Container } from '@mui/material';
 import {
-  HashRouter as Router,
+  BrowserRouter as Router,
   Switch,
   Route
 } from 'react-router-dom';
@@ -21,7 +21,7 @@ import CustomAlert from './components/CustomAlert';
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Router basename="/">
+      <Router basename="/react-auth-template">
         <AuthProvider>
           <AlertProvider>
             <Navbar />
